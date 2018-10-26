8f9e039bfdc47b348107c0e6865493a1850a3462
tayusa <37957375+tayusa@users.noreply.github.com>
2018-10-26 23:36:10 +0900

    [change] Always kill the Xephyr

--- jwm.sh
+++ jwm.sh
@@ -1,18 +1,17 @@
-# docker build -t $USER/jwm --build-arg DOCKER_UID=$(id -u) .
+# docker build -t $USER/ubuntu-jwm --build-arg DOCKER_UID=$(id -u) .
 
 function jwm() {
   docker info &> /dev/null \
     || { echo 'Is the docker daemon running?' && return 1 ;}
 
   [[ -e /tmp/.X11-unix/X1 ]] \
-    && local exists='true' \
     || Xephyr -wr -resizeable :1 &> /dev/null &
 
   docker run $@ \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v "/run/user/${UID}/pulse/native:/tmp/pulse/native" \
     -v "${HOME}/.config/pulse/cookie:/tmp/pulse/cookie" \
-    -it --rm "${USER}/jwm" &> /dev/null
+    -it --rm "${USER}/ubuntu-jwm" &> /dev/null
 
-  [[ -z ${exists} ]] && pkill Xephyr &> /dev/null
+  pkill Xephyr &> /dev/null
 }
